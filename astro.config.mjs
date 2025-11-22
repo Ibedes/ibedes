2a01268b6fd87ea5d9fddfe07b604aed362f5704
ibedestudio <ibedeskun@gmail.com>
2025-11-22 14:48:24 +0700

    update for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ibedes.xyz",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
