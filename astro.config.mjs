8ece3b734dfc3f066a55e1cca1938e6011807aab
ibedestudio <ibedeskun@gmail.com>
2025-11-29 08:18:40 +0700

    update ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,26 +7,25 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 import node from "@astrojs/node";
+
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ibedes.xyz",
   output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+
   image: {
     remotePatterns: [
       { protocol: "https", hostname: "**.susercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react(), sitemap()],
+  adapter: cloudflare(),
 });
